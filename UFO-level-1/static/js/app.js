17de922956471721ff1e4573bff0ba23c06bcaf8
Fareskazemi <Fareskazemi@live.com.au>
2022-06-23 17:58:27 +0800

    UFO-1 push

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,14 @@
+// IMPORTING FROM data.js
+var tableData = data;
+//Check Data (i)
+console.log(tableData);
+
+
+
+//TABLE
+// Selecting body
+var tableBody = d3.select9("tbody");
+//Function to clear table to edit/insert values
+function clearTable(DataInsert) {
+    
+}
