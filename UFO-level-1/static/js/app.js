eba10c529b6092d33891a6b836fef440ef508bd9
Fareskazemi <Fareskazemi@live.com.au>
2022-06-24 16:04:45 +0800

    UFO 1 final push

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,10 +5,30 @@ console.log(tableData);
 
 
 
-//TABLE
+//TABLE creation and insert
 // Selecting body
-var tableBody = d3.select9("tbody");
+var tableBody = d3.select("tbody");
 //Function to clear table to edit/insert values
 function clearTable(DataInsert) {
-    
+    tableBody.html("");
+    // Adding all rows and cells
+    DataInsert.forEach((ufosighting) => {
+        var row = tableBody.append("tr");
+        Object.entries(ufosighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+clearTable(data);
+
+//FILTER
+//Define function to filter
+function clickfilter() {
+    //Get reference to dateTime
+    var filterdate = d3.select("#datetime").property("value");
+    let filteredDATA = data;
+    if (filterdate) {filteredDATA = filteredDATA.filter(row => row.datetime === inputDate);}
+    clearTable(filteredDATA);
 }
+d3.selectAll("#filter-btn").on("click", clickfilter);
